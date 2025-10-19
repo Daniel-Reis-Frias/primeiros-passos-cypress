86451096bfc5eecc85f6d2331059f24200940a5d
Daniel-Reis-Frias <reisfriasd@gmail.com>
2025-10-19 04:57:49 -0300

    Adding userData.json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,33 +1,39 @@
+import userData from '../fixtures/userData.json'
+
+
 describe('Orange HRM tests', () => {
 
   const selectorsList = {
     usernameField: "[name='username']",
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
+    dashboardGrid: ".orangehrm-dashboard-grid",
     wrongCredentialAlert: ".oxd-alert"
   }
+
+
   it('Login - Success', () => {
     
     // Acessa o Site descrito no link
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login?lang=en')
     
     // Insere nome de usuário, senha e seleciona a opção de login
-    cy.get(selectorsList.usernameField).type('Admin')
-    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     
     // Garante que o usuário realmente chegou ao dashboard
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     
     // Confirma visualmente que o texto "Dashboard" está visível
-    cy.contains('h6', 'Dashboard')
+    cy.get(selectorsList.dashboardGrid)
   })
     it('Login - Fail', () => {
     
     
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login?lang=en')
-    cy.get(selectorsList.usernameField).type('test')
-    cy.get(selectorsList.passwordField).type('test')
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)
+    cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialAlert)
   })  
