c60357a7e541b0712900c0dd0eaebf52605a109a
Daniel-Reis-Frias <reisfriasd@gmail.com>
2025-10-23 08:51:37 -0300

    Select nationalityField added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -14,30 +14,34 @@ describe('Orange HRM tests', () => {
     lastNameField:"[name='lastName']",
     genericField:".oxd-input--active",
     dateField: "[placeholder='yyyy-dd-mm']",
+    nationalityField:".oxd-select-text--arrow",
+   // maritalStatus:
+    //dateOfBirth:
+    //genderField
     submitButton: "[type='submit']"
   }
  
   it.only('User Info Update - Success', () => {
-  // Acessa o site
+  
   cy.visit('/auth/login?lang=en')
 
-  // Garante a URL esperada
+
   cy.url().then(url => cy.log('URL atual:', url))
 
-  // Preenche credenciais
+  
   cy.get(selectorsList.usernameField,  { timeout: 10000 }).type(userData.userSuccess.username)
   cy.get(selectorsList.passwordField,  { timeout: 10000 }).type(userData.userSuccess.password)
   cy.get(selectorsList.loginButton).click()
 
-  // Aguarda login -> Dashboard
+  
   cy.location('pathname', { timeout: 10000 })
     .should('include', '/web/index.php/dashboard')
 
-  // Garante que o dashboard carregou
+  
   cy.get(selectorsList.dashboardGrid, { timeout: 10000 })
     .should('be.visible')
 
-  // Clica em My Info
+  
   cy.get(selectorsList.myInfoButton).should('be.visible').click()
 
   // Valida que o My Info abriu
@@ -46,15 +50,20 @@ describe('Orange HRM tests', () => {
 
   cy.contains('Personal Details',  { timeout: 10000 }).should('be.visible')
 
-  cy.get(selectorsList.firstNameField,  { timeout: 20000 }).clear().type('FirstName Test')
+  
+  cy.get(selectorsList.firstNameField,  { timeout: 20000 }).scrollIntoView()
+  .should('be.visible').clear().type('FirstName Test')
+  cy.wait(2000)
   cy.get(selectorsList.lastNameField,  { timeout: 20000 }).clear().type('LastNameTest')
   cy.get(selectorsList.genericField,  { timeout: 20000 }).eq(3).clear().type('Employee')
   cy.get(selectorsList.genericField,  { timeout: 20000 }).eq(4).clear().type('OtherIdTest')
   cy.get(selectorsList.genericField,  { timeout: 20000 }).eq(5).clear().type('DriversLicensceNumTest')
   cy.get(selectorsList.genericField).eq(6).clear().type('2025-10-22')
+  cy.get('.oxd-date-input-calendar').contains('Close').click({ force: true })
+  cy.get(selectorsList.nationalityField).eq(0).click()
+  cy.get('.oxd-select-dropdown').contains('.oxd-select-option', 'Brazilian').click()  
   cy.get(selectorsList.submitButton).eq(0).click()
   cy.get('body').should('contain', 'Successfully Updated')
-  cy.get('oxd-toast-close', { timeout: 20000 })
 
 })
 
